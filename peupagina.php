62eb1b502a0419d87f34dcfad69cd78d2c5c0cb4
root <root@carlosm07.fjeclot.net>
2019-03-08 17:55:52 +0100

    Commit 4

--- peupagina.php
+++ peupagina.php
@@ -8,16 +8,27 @@ Author: Carlos Guinovart Galofre
 Author URI: https://github.com/caguga1718daw2
 License: GNU GPLv3
 */
-function admin_menu_peu_pagina(){ 
-$titolPagina="Peu de pàgina per defecte";
-$titolEntrada="Peu de pàgina per defecte";
-$role="administrator";
-$id="peu_pagina_per_defecte";
-$nom_funcio="nouPeuPagDef";
-add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
+
+
+
+
+function admin_menu_peu_pagina(){
+	$titolPagina="Peu de pàgina per defecte";
+	$titolEntrada="Peu de pàgina per defecte";
+	$role="administrator";
+	$id="peu_pagina_per_defecte";
+	$nom_funcio="nouPeuPagDef";
+	add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio); 
 }
 function nouPeuPagDef(){
-include_once('formPeuPag.php');
+	include_once('formPeuPag.php');
+}
+function afegPeuPagDef(){
+    echo "<div style='color: red;
+    font-size: 10px;
+    margin: 10px;'>".get_option('footer_text')."</div>";
 }
 add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
+?>
 ?>
